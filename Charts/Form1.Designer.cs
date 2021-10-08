67f50c70a9051fd2482131a9c48aa03727f2b131
Cem TEZGELEN <92119083+cemtezgelen@users.noreply.github.com>
2021-10-08 08:14:43 +0300

    Add files via upload

---
+++ Charts/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Charts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(606, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "kız";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Charts/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Charts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=student;Integrated Security=TRUE");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        SqlDataReader dr;
+        DataSet ds = new DataSet();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float d1, d2, d3, toplam;
+            
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select cinsiyet from student";
+            //veri tabanında student adında veri tabanı ve student adında tablo var, tablolarda cinsiyet sütunu var, değerleri e veya k olacak
+            dr = cmd.ExecuteReader();
+            int erkeksayisi = 0;
+            int kizsayisi = 0;
+            while (dr.Read())
+            {
+                if (dr[0].ToString() == "e")
+                {
+                    erkeksayisi++;
+                }
+                else
+                {
+                    kizsayisi++;
+                }
+            }
+            int s1 = erkeksayisi;
+            int s2 = kizsayisi;
+            int s3 = 20; //s3 kaldırılıp 2 değer tutulabilir. 20, öylesine girildi, değişken olabilir
+            con.Close();
+            d1 = erkeksayisi;
+            d2 = kizsayisi;
+            d3 = s3;
+            toplam = d1 + d2+d3;
+            float pd1, pd2, pd3;
+            pd1 = (d1 / toplam) * 360;
+            pd2 = (d2 / toplam) * 360;
+            pd3 = (d3 / toplam) * 360;
+            Pen p = new Pen(Color.Black, 2);
+            Graphics g = this.CreateGraphics();
+            Rectangle r = new Rectangle(button1.Location.X + button1.Size.Width + 50, 50, 200, 200);
+            Brush b1 = new SolidBrush(Color.Green);
+            Brush b2 = new SolidBrush(Color.Red);
+            Brush b3 = new SolidBrush(Color.Blue);
+            g.Clear(Form1.DefaultBackColor);
+            g.DrawPie(p, r, 0, pd1);
+            g.FillPie(b1, r, 0, pd1);
+            g.DrawPie(p, r, pd1, pd2);
+            g.FillPie(b2, r, pd1, pd2);
+            g.DrawPie(p, r, pd1+pd2, pd3);
+            g.FillPie(b3, r, pd1+pd2, pd3);
+
+
+        }
+    }
+}
